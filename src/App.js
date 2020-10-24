0019308c12eb4cb640f3cc0287ab33c2b803f794
ANZania <zianastasia@yandex.ru>
2020-10-24 21:23:06 +0300

    Load project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './componets/Header';
+import Main from './componets/Main';
+import Features from './componets/Features';
+import Contact from './componets/Contact';
+import Footer from './componets/Footer';
+import '../src/css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    elementProps: {
+      names: [
+        'Первое', 'Второе', 'Третье', 'Четвертое'
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <>
+      <Header />
+      <Main />
+      <Features { ...this.state.elementProps}/>
+      <Contact />
+      <Footer />
+      </>
+    )
+  }
 }
 
 export default App;
